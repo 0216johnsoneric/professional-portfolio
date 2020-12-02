ce1860f40cf6647110d5539d95e786fb9628271a
Eric Johnson <ericjohnson0216@icloud.com>
2020-12-01 20:51:17 -0500

    deploy

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio", 
+{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
